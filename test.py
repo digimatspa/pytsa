f6a2bafde4b4a3f2f3510cd5d41a50d1cab7c65e
Paolo Bitonto <paolo.bitonto@digimat.it>
2025-01-30 19:22:38 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,47 +1,57 @@
 import pytsa
 from pathlib import Path
-import matplotlib.pyplot as plt
 
-# Global geographic search area.
-# Outside these bounds, no search will be commenced
-frame = pytsa.BoundingBox(
-    LATMIN = 51.85,
-    LATMAX = 60.49,
-    LONMIN = 4.85,
-    LONMAX = 14.3,
-)
+def preprocess(df):
+    return df[df["speed"] > 0]
 
-# File containing AIS messages
-# Replace with the path to your own data ---v
-dynamic_data = Path("path/to/dynamic_data.csv")
-static_data = Path("path/to/static_data.csv")
+if __name__ == '__main__':
+    # Global geographic search area.
+    # Outside these bounds, no search will be commenced
+    frame = pytsa.BoundingBox(
+        LATMIN=55.196,  # [°N]
+        LATMAX=57.064,  # [°N]
+        LONMIN=4.994,   # [°E]
+        LONMAX=9.432,   # [°E]
+    )
 
-# Instantiate the search agent with the source file 
-# and the search area
-search_agent = pytsa.SearchAgent(
-    dynamic_paths=dynamic_data,
-    static_paths=static_data,
-    frame=frame,
-    preprocessor= lambda df: df[df["speed"] > 0]
-)
+    coordinates = {'latitude': 56.7780335777419, 'longitude': 8.53576037485539}
 
-# Provide a position and time for which the search
-# will be carried out
-tpos = pytsa.TimePosition(
-    timestamp="2021-04-01T08:00:00Z",
-    lat=54.00,
-    lon=8.26
-)
+    # File containing AIS messages
+    # Replace with the path to your own data ---v
+    dynamic_data = Path("../test/dynamic/2025_01_19.csv")
+    static_data = Path("../test/static/2025_01_19.csv")
 
-# Track the target vessels for 100 iterations
-# of 3 seconds each and print the observed
-# position, speed and course
-for _ in range(0,100):
-    # Search for TargetVessels
-    tpos.timestamp += 3 # 3 seconds
-    target_ships = search_agent.freeze(tpos,search_radius=40)
+    # Instantiate the search agent with the source file
+    # and the search area
+    search_agent = pytsa.SearchAgent(
+        dynamic_paths=dynamic_data,
+        static_paths=static_data,
+        frame=frame,
+        preprocessor=preprocess
+    )
+
+    found = False
+    print("Searching", coordinates['latitude'], coordinates['longitude'])
+
+    # Provide a position and time for which the search
+    # will be carried out
+    tpos = pytsa.TimePosition(
+        timestamp="2025-01-19T05:49:16.000Z",
+        lat=coordinates['latitude'],
+        lon=coordinates['longitude']
+    )
+
+    # Search for TargetVessels with
+    # default settings:
+    #   Linear interpolation,
+    #   20 nm search radius
+    target_ships = search_agent.freeze(tpos, search_radius=20, alpha=0.02)
 
     # Extract the current position, speed and
     # course for all found target vessels.
     for ship in target_ships.values():
-        print(ship.observe())
+        print("Found", ship.mmsi, ship.observe())
+        found = True
+
+    if not found:
+        print("Not found")
